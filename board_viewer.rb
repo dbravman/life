c2bcfcf6638a4c9ddf3241e1be1a67a03e849d66
Danny Bravman <dbravman@gmail.com>
2016-07-15 12:34:15 -0500

    add ruby racer code

--- board_viewer.rb
+++ board_viewer.rb
@@ -2,6 +2,7 @@ require_relative "cell_viewer"
 
 module GameViewer
   def self.show(game)
+    reset_screen
     game.board.each do |cell|
       CellViewer.show(cell)
       if cell.x == game.width-1
@@ -9,4 +10,19 @@ module GameViewer
       end
     end
   end 
+
+  def reset_screen
+    clear_screen
+    move_to_home
+  end
+
+  # Clears the content on the screen. Ah, a fresh canvas.
+  def clear_screen
+    print "\e[2J"
+  end
+
+  # Moves the insert point in the terminal back to the upper left.
+  def move_to_home
+    print "\e[H"
+  end
 end
